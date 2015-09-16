7900fbb10de8d3303da341c5efa98b793ee295b2
Paul Annesley <paul@annesley.cc>
2015-09-16 16:04:08 -0700

    SigningString avoids Symfony's getQueryString() mangling.
    Symfony's `getQueryString()` normalizes the ordering of query string
    parameters. This meant that signing/verification of URLs with
    out-of-order query strings was incorrect.

---
+++ src/SymfonyRequestMessage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace HttpSignatures;
+
+class SymfonyRequestMessage
+{
+    private $request;
+    public $headers;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+        $this->headers = $request->headers;
+    }
+
+    public function getPathInfo()
+    {
+        return $this->request->getPathInfo();
+    }
+
+    public function getQueryString()
+    {
+        // Symfony\Component\HttpFoundation\Request::getQueryString() is not
+        // suitable for HTTP signatures as it mangles the query string.
+        return $this->request->server->get('QUERY_STRING');
+    }
+
+    public function getMethod()
+    {
+        return $this->request->getMethod();
+    }
+}
