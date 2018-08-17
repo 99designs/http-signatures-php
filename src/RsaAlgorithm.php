189c3ead994c15480bef71a43d5f0c0a48f2d57b
Liam Dennehy <liam@wiemax.net>
2018-08-18 01:08:46 +0200

    Switch for different SHA2 hash lengths

--- src/RsaAlgorithm.php
+++ src/RsaAlgorithm.php
@@ -31,11 +31,28 @@ class RsaAlgorithm implements AlgorithmInterface
      */
     public function sign($key, $data)
     {
+        switch ($this->digestName) {
+          case 'sha224':
+            $algo = OPENSSL_ALGO_SHA224;
+            break;
+          case 'sha256':
+            $algo = OPENSSL_ALGO_SHA256;
+            break;
+          case 'sha384':
+            $algo = OPENSSL_ALGO_SHA384;
+            break;
+          case 'sha512':
+            $algo = OPENSSL_ALGO_SHA512;
+            break;
+          default:
+            throw new Exception($this->digestName . " is not a supported hash format for RSA");
+            break;
+        }
         if ( ! openssl_get_privatekey($key) ) {
           throw new Exception("OpenSSL doesn't understand the supplied key (not valid or not found)");
         }
         $signature="";
-        openssl_sign($data, $signature, $key, OPENSSL_ALGO_SHA256);
+        openssl_sign($data, $signature, $key, $algo);
         return $signature;
     }
 }
