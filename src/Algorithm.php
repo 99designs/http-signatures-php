473cbadeff6a49e50f7554370174aa81c8de08b3
Liam Dennehy <liam@wiemax.net>
2018-08-18 15:12:08 +0200

    Restrict RSA algorithm selection to those in the standard

--- src/Algorithm.php
+++ src/Algorithm.php
@@ -20,14 +20,11 @@ abstract class Algorithm
         case 'hmac-sha256':
             return new HmacAlgorithm('sha256');
             break;
+        case 'rsa-sha1':
+            return new RsaAlgorithm('sha1');
+            break;
         case 'rsa-sha256':
             return new RsaAlgorithm('sha256');
-            break;
-        case 'rsa-sha384':
-            return new RsaAlgorithm('sha384');
-            break;
-        case 'rsa-sha512':
-            return new RsaAlgorithm('sha512');
             break;
         default:
             throw new Exception("No algorithm named '$name'");
