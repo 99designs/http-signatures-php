01e949fb0094d99499e598f46fd189f929c8266f
Liam Dennehy <liam@wiemax.net>
2018-08-19 22:54:27 +0200

    RSA Verification tests

--- tests/RsaContextTest.php
+++ tests/RsaContextTest.php
@@ -12,19 +12,24 @@ class RsaContextTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->context = new Context([
+        $this->sha1context = new Context([
             'keys' => ['rsa1' => TestKeys::rsaKey],
             'algorithm' => 'rsa-sha1',
             'headers' => ['(request-target)', 'date'],
         ]);
+        $this->sha256context = new Context([
+            'keys' => ['rsa1' => TestKeys::rsaKey],
+            'algorithm' => 'rsa-sha256',
+            'headers' => ['(request-target)', 'date'],
+        ]);
     }
 
-    public function testSigner()
+    public function testSha1Signer()
     {
         $message = new Request('GET', '/path?query=123', ['date' => 'today', 'accept' => 'llamas']);
-        $message = $this->context->signer()->sign($message);
 
-        $expectedString = implode(',', [
+        $message = $this->sha1context->signer()->sign($message);
+        $expectedSha1String = implode(',', [
             'keyId="rsa1"',
             'algorithm="rsa-sha1"',
             'headers="(request-target) date"',
@@ -37,24 +42,33 @@ class RsaContextTest extends \PHPUnit_Framework_TestCase
         ]);
 
         $this->assertEquals(
-            $expectedString,
+            $expectedSha1String,
             $message->getHeader('Signature')[0]
         );
 
-        $this->assertEquals(
-            'Signature ' . $expectedString,
-            $message->getHeader('Authorization')[0]
-        );
     }
 
-    public function testVerifier()
+    public function testSha256Signer()
     {
-        $message = $this->context->signer()->sign(new Request('GET', '/path?query=123', [
-            'Signature' => 'keyId="rsa1",algorithm="rsa-sha1",headers="date",signature="x"',
-            'Date' => 'x',
-        ]));
+        $message = new Request('GET', '/path?query=123', ['date' => 'today', 'accept' => 'llamas']);
 
-        // assert it works without errors; correctness of results tested elsewhere.
-        $this->assertTrue(is_bool($this->context->verifier()->isValid($message)));
-    }
+        $message = $this->sha256context->signer()->sign($message);
+        $expectedSha256String = implode(',', [
+            'keyId="rsa1"',
+            'algorithm="rsa-sha256"',
+            'headers="(request-target) date"',
+            'signature="WGIegQCC3GEwxbkuXtq67CAqeDhkwblxAH2uoDx5kfWurhLRA5WB' .
+            'FDA/aktsZAjuUoimG1w4CGxSecziER1ez44PBlHP2fCW4ArLgnQgcjkdN2cOf/g' .
+            'j0OVL8s2usG4o4tud/+jjF3nxTxLl3HC+erBKsJakwXbw9kt4Cr028BToVfNXsW' .
+            'oMFpv0IjcgBH2V41AVlX/mYBMMJAihBCIcpgAcGrrxmG2gkfvSn09wtTttkGHft' .
+            'PIp3VpB53zbemlJS9Yw3tmmHr6cvWSXqQy/bTsEOoQJ2REfn5eiyzsJu3GiOpiI' .
+            'LK67i/WH9moltJtlfV57TV72cgYtjWa6yqhtFg=="',
+        ]);
+
+        $this->assertEquals(
+            $expectedSha256String,
+            $message->getHeader('Signature')[0]
+        );
+      }
+
 }
