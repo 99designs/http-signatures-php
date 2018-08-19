15b6da9d1c29c9e842d6f66b6d89986b80278b15
Liam Dennehy <liam@wiemax.net>
2018-08-19 13:16:26 +0200

    Add RSA to SignatureParametersTest

--- tests/SignatureParametersTest.php
+++ tests/SignatureParametersTest.php
@@ -4,12 +4,13 @@ namespace HttpSignatures\tests;
 
 use HttpSignatures\HeaderList;
 use HttpSignatures\HmacAlgorithm;
+use HttpSignatures\RsaAlgorithm;
 use HttpSignatures\Key;
 use HttpSignatures\SignatureParameters;
 
 class SignatureParametersTest extends \PHPUnit_Framework_TestCase
 {
-    public function testToString()
+    public function testHmacToString()
     {
         $key = new Key('pda', 'secret');
         $algorithm = new HmacAlgorithm('sha256');
@@ -28,6 +29,29 @@ class SignatureParametersTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             'keyId="pda",algorithm="hmac-sha256",headers="(request-target) date",signature="dGhlc2lnbmF0dXJl"',
+            $sp->string()
+        );
+    }
+
+    public function testRsaToString()
+    {
+        $key = new Key('pda', TestKeys::rsaKey);
+        $algorithm = new RsaAlgorithm('sha256');
+        $headerList = new HeaderList(['(request-target)', 'date']);
+
+        $signature = $this->getMockBuilder('HttpSignatures\Signature')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $signature
+            ->expects($this->any())
+            ->method('string')
+            ->will($this->returnValue('thesignature'));
+
+        $sp = new SignatureParameters($key, $algorithm, $headerList, $signature);
+
+        $this->assertEquals(
+            'keyId="pda",algorithm="rsa-sha256",headers="(request-target) date",signature="dGhlc2lnbmF0dXJl"',
             $sp->string()
         );
     }
