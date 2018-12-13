a49b983f9c77ec470b22df6e5aadea3edf6ca508
Mark Ogilvie <mogilvie@users.noreply.github.com>
2018-12-13 15:34:33 +0000

    Add RSA-SHA512 Algorithm

--- src/Algorithm.php
+++ src/Algorithm.php
@@ -20,6 +20,9 @@ abstract class Algorithm
         case 'hmac-sha256':
             return new HmacAlgorithm('sha256');
             break;
+        case 'rsa-sha512':
+            return new RsaAlgorithm('sha512');
+            break;
         default:
             throw new Exception("No algorithm named '$name'");
             break;
