e4c263d3937fc47ccda00f4c7212302be1636f22
Mark Ogilvie <mogilvie@users.noreply.github.com>
2021-05-28 14:52:56 +0100

    Create RsaAlgorithm.php

---
+++ src/RsaAlgorithm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HttpSignatures;
+
+class RsaAlgorithm implements AlgorithmInterface
+{
+    /** @var string */
+    private $digestName;
+
+    /**
+     * @param string $digestName
+     */
+    public function __construct($digestName)
+    {
+        $this->digestName = $digestName;
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        return sprintf('rsa-%s', $this->digestName);
+    }
+
+    /**
+     * @param string $key
+     * @param string $data
+     *
+     * @return string
+     */
+    public function sign($privateKey, $data)
+    {
+        openssl_sign($data, $signature, $privateKey, 'RSA-SHA512');
+
+        return $signature;
+    }
+}
